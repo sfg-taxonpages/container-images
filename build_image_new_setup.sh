aa3f2272e12c3d2cc5d4c269378c8928838d3655
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2026-04-09 14:54:06 -0300

    Orthoptera new-setup

---
+++ build_image_new_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+
+rm -rf tmp
+git clone https://github.com/sfg-taxonpages/$2.git tmp
+cd tmp 
+git checkout $3
+cd .. 
+cp Dockerfile .dockerignore tmp 
+docker build -t $1/sfg-taxonpages/$2 tmp 
+rm -rf tmp
+
