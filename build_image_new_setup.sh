a8d836672a78795bd203caedca4878e9dfb2794a
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2026-04-13 15:26:27 -0300

    Remove config/router.yml in build script
    Remove router.yml file during image build setup.

--- build_image_new_setup.sh
+++ build_image_new_setup.sh
@@ -6,6 +6,7 @@ rm -rf tmp
 git clone https://github.com/sfg-taxonpages/$2.git tmp
 cd tmp 
 git checkout $3
+rm config/router.yml
 cd .. 
 cp Dockerfile .dockerignore tmp 
 docker build -t $1/sfg-taxonpages/$2 tmp 
